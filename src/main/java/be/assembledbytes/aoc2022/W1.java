86e55fc8e0f16959a77ee4941a305a38edbf5842
Alexander Bollaert <alexander.bollaert@gmail.com>
2022-12-01 21:30:44 +0100

    Cleanup

--- src/main/java/be/assembledbytes/aoc2022/W1.java
+++ src/main/java/be/assembledbytes/aoc2022/W1.java
@@ -3,7 +3,6 @@ package be.assembledbytes.aoc2022;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -46,6 +45,8 @@ public class W1 {
 
         elves.sort(Collections.reverseOrder(Comparator.comparing(Elf::countCalories)));
 
-        System.out.println(elves.get(0).countCalories() + elves.get(1).countCalories() + elves.get(2).countCalories());
+        System.out.println(elves.get(0).countCalories() +
+                           elves.get(1).countCalories() +
+                           elves.get(2).countCalories());
     }
 }
